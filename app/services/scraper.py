6a10a27737f611bc9f365f182441711b07a9b3d8
div-tom1506 <divyanshuk1506@gmail.com>
2025-06-18 20:58:14 +0530

    comment

--- app/services/scraper.py
+++ app/services/scraper.py
@@ -2,3 +2,5 @@
 async def scrape_all(user_links):
     pass
 
+# to see logic and tool to implement scraping in linkedin
+# to use twitter link not for scraping, just to attach it in resume and website
